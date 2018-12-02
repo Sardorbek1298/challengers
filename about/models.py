24e99df45586c4a59d6986d839b3c7465f349978
Dilshod Bakhtiyorov <baxtiyorovdilshod305@mail.ru>
2018-12-02 12:09:03 +0500

    make it better

--- about/models.py
+++ about/models.py
@@ -5,6 +5,7 @@ class Hospital(models.Model):
     name = models.CharField(max_length=100)
     number = models.CharField(max_length=100)
     creater = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
